9b90ca0de4079bc4cfc7b58bc6a9e943df052a31
Zainabessa <zainabesa2002@gmail.com>
2025-04-09 15:59:40 +0200

    update data

--- smart_home.sql
+++ smart_home.sql
@@ -135,3 +135,137 @@ CREATE TABLE `subscription` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
+ALTER TABLE `account`
+    ADD PRIMARY KEY (`accountID`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `phoneNumber` (`phoneNumber`);
+
+ALTER TABLE `consumptionlog`
+    ADD PRIMARY KEY (`readingNo`),
+
+ALTER TABLE `emergencyentity`
+    ADD PRIMARY KEY (`emergencyID`),
+  ADD KEY `inventoryDeviceID` (`inventoryDeviceID`);
+
+ALTER TABLE `emergencyincident`
+    ADD PRIMARY KEY (`containsNo`),
+  ADD KEY `inventoryDeviceID` (`inventoryDeviceID`),
+  ADD KEY `emergencyID` (`emergencyID`);
+
+ALTER TABLE `home`
+    ADD PRIMARY KEY (`homeID`),
+  ADD KEY `accountID` (`accountID`);
+
+ALTER TABLE `inventorydevice`
+    ADD PRIMARY KEY (`inventoryDeviceID`),
+  ADD KEY `homeID` (`homeID`),
+  ADD KEY `deviceID` (`deviceID`);
+
+ALTER TABLE `member`
+    ADD PRIMARY KEY (`memberID`),
+  ADD KEY `accountID` (`accountID`);
+
+
+ALTER TABLE `message`
+    ADD PRIMARY KEY (`messageNumber`),
+  ADD KEY `accountID` (`accountID`),
+  ADD KEY `recipientID` (`recipientID`),
+  ADD KEY `previousMessageNo` (`previousMessageNo`);
+
+ALTER TABLE `operationschedule`
+    ADD PRIMARY KEY (`scheduleID`),
+  ADD KEY `inventoryDeviceID` (`inventoryDeviceID`);
+
+ALTER TABLE `smartacsetting`
+    ADD PRIMARY KEY (`acSettingID`),
+  ADD KEY `inventoryDeviceID` (`inventoryDeviceID`);
+
+ALTER TABLE `smartdevice`
+    ADD PRIMARY KEY (`deviceID`);
+
+ALTER TABLE `smartlightsetting`
+    ADD PRIMARY KEY (`lightSettingID`),
+  ADD KEY `inventoryDeviceID` (`inventoryDeviceID`);
+
+ALTER TABLE `subscription`
+    ADD PRIMARY KEY (`subscriptionID`),
+  ADD KEY `accountID` (`accountID`);
+
+ALTER TABLE `account`
+    MODIFY `accountID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+ALTER TABLE `consumptionlog`
+    MODIFY `readingNo` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+ALTER TABLE `emergencyentity`
+    MODIFY `emergencyID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+ALTER TABLE `emergencyincident`
+    MODIFY `containsNo` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+ALTER TABLE `home`
+    MODIFY `homeID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+ALTER TABLE `inventorydevice`
+    MODIFY `inventoryDeviceID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+ALTER TABLE `member`
+    MODIFY `memberID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+ALTER TABLE `message`
+    MODIFY `messageNumber` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+ALTER TABLE `operationschedule`
+    MODIFY `scheduleID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+ALTER TABLE `smartacsetting`
+    MODIFY `acSettingID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+ALTER TABLE `smartdevice`
+    MODIFY `deviceID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+ALTER TABLE `smartlightsetting`
+    MODIFY `lightSettingID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+ALTER TABLE `subscription`
+    MODIFY `subscriptionID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+ALTER TABLE `consumptionlog`
+    ADD CONSTRAINT `consumptionlog_ibfk_1` FOREIGN KEY (`inventoryDeviceID`) REFERENCES `inventorydevice` (`inventoryDeviceID`) ON DELETE CASCADE;
+
+ALTER TABLE `emergencyentity`
+    ADD CONSTRAINT `emergencyentity_ibfk_1` FOREIGN KEY (`inventoryDeviceID`) REFERENCES `inventorydevice` (`inventoryDeviceID`) ON DELETE CASCADE;
+
+
+ALTER TABLE `emergencyincident`
+    ADD CONSTRAINT `emergencyincident_ibfk_1` FOREIGN KEY (`inventoryDeviceID`) REFERENCES `inventorydevice` (`inventoryDeviceID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `emergencyincident_ibfk_2` FOREIGN KEY (`emergencyID`) REFERENCES `emergencyentity` (`emergencyID`) ON DELETE CASCADE;
+
+ALTER TABLE `home`
+    ADD CONSTRAINT `home_ibfk_1` FOREIGN KEY (`accountID`) REFERENCES `account` (`accountID`) ON DELETE CASCADE;
+
+ALTER TABLE `inventorydevice`
+    ADD CONSTRAINT `inventorydevice_ibfk_1` FOREIGN KEY (`homeID`) REFERENCES `home` (`homeID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `inventorydevice_ibfk_2` FOREIGN KEY (`deviceID`) REFERENCES `smartdevice` (`deviceID`) ON DELETE CASCADE;
+
+ALTER TABLE `member`
+    ADD CONSTRAINT `member_ibfk_1` FOREIGN KEY (`accountID`) REFERENCES `account` (`accountID`) ON DELETE CASCADE;
+
+ALTER TABLE `message`
+    ADD CONSTRAINT `message_ibfk_1` FOREIGN KEY (`accountID`) REFERENCES `account` (`accountID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `message_ibfk_2` FOREIGN KEY (`recipientID`) REFERENCES `account` (`accountID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `message_ibfk_3` FOREIGN KEY (`previousMessageNo`) REFERENCES `message` (`messageNumber`) ON DELETE SET NULL;
+
+ALTER TABLE `operationschedule`
+    ADD CONSTRAINT `operationschedule_ibfk_1` FOREIGN KEY (`inventoryDeviceID`) REFERENCES `inventorydevice` (`inventoryDeviceID`) ON DELETE CASCADE;
+
+ALTER TABLE `smartacsetting`
+    ADD CONSTRAINT `smartacsetting_ibfk_1` FOREIGN KEY (`inventoryDeviceID`) REFERENCES `inventorydevice` (`inventoryDeviceID`) ON DELETE CASCADE;
+
+ALTER TABLE `smartlightsetting`
+    ADD CONSTRAINT `smartlightsetting_ibfk_1` FOREIGN KEY (`inventoryDeviceID`) REFERENCES `inventorydevice` (`inventoryDeviceID`) ON DELETE CASCADE;
+
+ALTER TABLE `subscription`
+    ADD CONSTRAINT `subscription_ibfk_1` FOREIGN KEY (`accountID`) REFERENCES `account` (`accountID`) ON DELETE CASCADE;
+COMMIT;
+
+
